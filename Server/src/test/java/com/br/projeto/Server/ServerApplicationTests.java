c08cfb091c45627e6abe340c9e988ae574ca1fa6
arkauser <arkauser12@gmail.com>
2019-09-05 16:37:51 -0300

    initial commit

---
+++ Server/src/test/java/com/br/projeto/Server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projeto.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
